8cfec5f2787dcc538d608df2e826030d0c66daf7
Jenxassy <Jenxassy@sina.com>
2018-10-17 13:32:28 +0800

    s

--- testA/src/main/java/com/MyApplication.java
+++ testA/src/main/java/com/MyApplication.java
@@ -1,27 +1,13 @@
 package com;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
-@EnableTransactionManagement
 public class MyApplication {
 	// 启动的时候要注意，由于我们在controller中注入了RestTemplate，所以启动的时候需要实例化该类的一个实例
 	@Autowired
